ec9230a57082eae0445b92ee668a159daf6c338e
MC-Dabbz <96742572+MC-Dabbz@users.noreply.github.com>
2024-10-27 16:56:48 +0200

    Update streamlit_project.py

--- streamlit_project.py
+++ streamlit_project.py
@@ -42,7 +42,7 @@ st.write(f"Number of divorced individuals: {marital_status_counts.get('Divorced'
 
 # Income Distribution by Gender
 st.subheader("Income Distribution by Gender")
-income_by_gender = df.groupby('Favorite_Color')['Income_(USD)'].mean()
+income_by_gender = df.groupby('Education_Level')['Income_(USD)'].mean()
 st.bar_chart(income_by_gender)
 
 
